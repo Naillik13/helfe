d1d7e49500cb1c9ccd4027782996b47f80c1df67
Naillik13 <killian.galea0@orange.fr>
2019-12-16 16:04:28 +0100

    [FEAT] Clean auth

--- App.js
+++ App.js
@@ -11,102 +11,10 @@ import MapScreen from "./src/screens/MapScreen";
 import MoreScreen from "./src/screens/MoreScreen";
 import AlarmScreen from "./src/screens/AlarmScreen";
 
-const UserStack = createStackNavigator({
-  Profile: UserScreen
-});
+import AppNavigation from "./navigation/AppNavigation";
 
-UserStack.navigationOptions = {
-  tabBarIcon: ({ focused }) => (
-      <TabBarIcon
-          focused={focused}
-          name={
-            Platform.OS === 'ios'
-                ? `ios-person`
-                : 'md-person'
-          }
-      />
-  ),
-};
 
-UserStack.path = '';
-
-const MapStack = createStackNavigator({
-  Map: MapScreen
-});
-
-MapStack.navigationOptions = {
-  tabBarIcon: ({ focused }) => (
-      <TabBarIcon
-          focused={focused}
-          name={
-            Platform.OS === 'ios'
-                ? `ios-map`
-                : 'md-map'
-          }
-      />
-  ),
-};
-
-MapStack.path = '';
-
-const AlarmStack = createStackNavigator({
-  Alarm: AlarmScreen
-});
-
-AlarmStack.navigationOptions = {
-  tabBarIcon: ({ focused }) => (
-      <TabBarIcon
-          focused={focused}
-          name={
-            Platform.OS === 'ios'
-                ? `ios-alert`
-                : 'md-alert'
-          }
-      />
-  ),
-};
-
-AlarmStack.path = '';
-
-const MoreStack = createStackNavigator({
-  More: MoreScreen
-});
-
-MoreStack.navigationOptions = {
-  tabBarIcon: ({ focused }) => (
-      <TabBarIcon
-          focused={focused}
-          name={
-            Platform.OS === 'ios'
-                ? `ios-information`
-                : 'md-information'
-          }
-      />
-  ),
-};
-
-MoreStack.path = '';
 
 export default createAppContainer(
-    createBottomTabNavigator(
-        {
-          User: UserStack,
-          Alarm: AlarmStack,
-          Map: MapStack,
-          More: MoreStack
-
-        },
-        {
-            tabBarOptions: {
-              showLabel: false, // hide labels
-              style: {
-                  backgroundColor: Colors.tabBar,
-              },
-              tabStyle: {
-                  backgroundColor: Colors.tabBar,
-              },
-            },
-            initialRouteName: "User"
-        }
-    )
+    AppNavigation
 );
