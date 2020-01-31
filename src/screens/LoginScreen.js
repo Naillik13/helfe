174cbdafca266c89907a4f9998a8eba301c39e6c
Isaac Gros <isaac.gros888@gmail.com>
2020-01-31 15:21:42 +0100

    [FIX] Design of login page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, TextInput, Image, TouchableOpacity, Text, StyleSheet} from 'react-native'
+import {View, TextInput, Image, TouchableOpacity, Text, StyleSheet, Dimensions} from 'react-native'
 import firebase from "firebase";
 import {onLogin} from '../Auth'
 
@@ -33,30 +33,39 @@ export default class LoginScreen extends React.Component {
 
     };
 
+    static navigationOptions = () => ({
+        header: null,
+    });
+
     render(){
         return(
-            <View style={{}}>
+            <View style={styles.container}>
                 <Image style={styles.logo}
                  source={require('../../assets/logo.png')}/>
                 <TextInput
                     style={styles.input}
+                    placeholderTextColor={placeHolderColor}
+                    keyboardType='email-address'
                     placeholder='Email'
                     onChangeText={(email) => this.setState({email: email})}/>
                 <TextInput
-                    style={[styles.input, { marginTop: 30, marginBottom: 75}]}
+                    style={styles.input}
+                    placeholderTextColor={placeHolderColor}
                     placeholder='Password'
                     secureTextEntry={true}
                     password={true}
                     onChangeText={(password) => this.setState({password: password})}/>
 
                 <TouchableOpacity
-                    style={[styles.button, {marginBottom: 30}]}
+                    style={styles.button}
+                    activeOpacity={0.5}
                     onPress={() => this._login(this.state.email, this.state.password)}>
                     <Text style={styles.buttonText}>Login</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                     style={styles.button}
+                    activeOpacity={0.5}
                     onPress={() => this.props.navigation.navigate('Register')}>
                     <Text style={styles.buttonText}>Register</Text>
                 </TouchableOpacity>
@@ -65,36 +74,51 @@ export default class LoginScreen extends React.Component {
     }
 }
 
+// Screen sizes
+const viewWidth = Math.round(Dimensions.get('window').width);
+const viewHeight = Math.round(Dimensions.get('window').height);
+
 const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        flexGrow: 1
+    },
     logo: {
         alignSelf: 'center',
-        height: 250,
-        width: 250,
+        height: 150,
+        width: 150,
         marginTop: 80,
-        marginBottom: 75,
+        marginBottom: 50,
         borderRadius: 25
     },
     input: {
-        height: 40,
-        marginLeft: 70,
-        marginRight: 70,
-        paddingLeft: 10,
+        width: (viewWidth / 100 * 75),
+        alignSelf: 'center',
+        marginBottom: 25,
         backgroundColor: '#b3cdfb',
         borderRadius: 25,
-        fontSize: 18
+        fontSize: 16,
+        padding: 5,
+        paddingLeft: 20,
+        fontFamily: "montserrat-regular"
     },
     button: {
-        height: 42,
         backgroundColor: '#212580',
         marginLeft: 100,
         marginRight: 100,
+        marginTop: 25,
         borderRadius: 25
     },
     buttonText: {
         color:"white",
         textAlign: "center",
-        fontSize: 18,
-        marginVertical: 10
+        fontSize: 16,
+        marginVertical: 10,
+        padding: 3,
+        fontFamily: "montserrat-semibold"
     }
-
 });
+
+const placeHolderColor = "#555555"
